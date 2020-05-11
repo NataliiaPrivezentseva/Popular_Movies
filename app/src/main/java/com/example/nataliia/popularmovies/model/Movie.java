c89d3f8c1710f451c3e6c3767833e7873859dc8f
Nataliia Privezentseva <nataliia.privezentseva@gmail.com>
2020-05-11 15:24:51 +0200

    Create Movie class

---
+++ app/src/main/java/com/example/nataliia/popularmovies/model/Movie.java
@@ -0,0 +1,137 @@
+package com.example.nataliia.popularmovies.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class Movie {
+
+    private double popularity;
+    private double voteCount;
+    private boolean video;
+    private String posterPath;
+    private long id;
+    private boolean adult;
+    private String backdropPath;
+    private String originalLanguage;
+    private String originalTitle;
+    private List<Integer> genreIds;
+    private String title;
+    private int voteAverage;
+    private String overview;
+    private String releaseDate;
+
+    Movie(){
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public double getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(double voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(int voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
